0b5913a3ed295de6aef54bdeef6ccce2f057b5da
justtype <lehotsky.michal9@gmail.com>
2020-01-22 23:08:30 +0100

    Small changes to key control

--- src/KeyControl.java
+++ src/KeyControl.java
@@ -4,18 +4,18 @@ import java.awt.event.KeyEvent;
 public class KeyControl extends KeyAdapter {
     private Star selected;
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void keyPressed(KeyEvent e) {
         if(e.getKeyCode() == KeyEvent.VK_LEFT){
-            selected.modVelocity(-1, 0);
+            selected.modVelocity(-5, 0);
         }
         if(e.getKeyCode() == KeyEvent.VK_RIGHT){
-            selected.modVelocity(1, 0);
+            selected.modVelocity(5, 0);
         }
         if(e.getKeyCode() == KeyEvent.VK_UP){
-            selected.modVelocity(0, -1);
+            selected.modVelocity(0, -5);
         }
         if(e.getKeyCode() == KeyEvent.VK_DOWN){
-            selected.modVelocity(0, 1);
+            selected.modVelocity(0, 5);
         }
     }
 
